ca2541dcc4c8c54d3e8310ff407e3845a2c9e993
sunhao <2632430651@qq.com>
2017-04-28 17:08:22 +0800

    upload test

--- src/test/java/test/TestDicGen.java
+++ src/test/java/test/TestDicGen.java
@@ -8,6 +8,7 @@ import pers.badger.dic.HostDicGen;
 public class TestDicGen {
     public static void main(String[] args) {
         HostDicGen hostDicGen = new HostDicGen("172.16.1.0-172.16.250.250");
+        System.out.println();
         hostDicGen.process();
     }
 }
